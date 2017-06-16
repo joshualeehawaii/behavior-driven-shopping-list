c37b3f497dcc88111782599091099b84914ffba9
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-06-15 17:49:14 -1000

    added the 4th test and it passes

--- test/shopping_list_test.spec.js
+++ test/shopping_list_test.spec.js
@@ -7,23 +7,25 @@ var ShoppingListItem = require('../js/shopping_list_item.js');
 describe('ShoppingListItem Class', function(){
   var testVar = {};
   beforeEach(function() {
-    testVar = new ShoppingListItem('thing', 'it is stuff');
+    testVar = new ShoppingListItem('eggs', 'protein', 'true');
   });
-
+  //ShoppingListItem is a class
   it('should be a class', function(){
     var classTest = testVar instanceof ShoppingListItem;
     expect(classTest).to.equal(true);
-
   });
-
+  //ShoppingListItem has a name
   it('should have a name', function(){
-    expect(testVar.name).to.equal('thing');
+    expect(testVar.name).to.equal('eggs');
   });
-
+  //ShoppingListItem has a description
   it('should have a description', function(){
-    expect(testVar.description).to.equal('it is stuff');
+    expect(testVar.description).to.equal('protein');
   });
 
-
+  //ShoppingListItem has a property named is_done
+  it('should have a property named is_done', function(){
+    expect(testVar.is_done).to.equal('true');
+  });
 
 });
