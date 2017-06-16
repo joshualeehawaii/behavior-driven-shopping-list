de54f00774620982653f0377107aa2fe501212b0
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-06-15 17:48:40 -1000

    added the 4th the test, and is passing

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -1,14 +1,11 @@
 /*jshint esversion: 6 */
 
+//Creating the class Shopping List Item
 class ShoppingListItem {
-
-  constructor(name, description) {
-
+  constructor(name, description, is_done) {
     this.name = name;
     this.description = description;
-
+    this.is_done = is_done;
   }
-
 }
-
 module.exports = ShoppingListItem;
