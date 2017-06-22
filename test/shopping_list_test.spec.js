20e0cd215afbac965a9091dcd5ed9815ff2f7055
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-06-21 17:57:05 -1000

    added the method for render

--- test/shopping_list_test.spec.js
+++ test/shopping_list_test.spec.js
@@ -140,4 +140,26 @@ describe('ShoppingList Class', function(){
 
   });
 
+  it('should have a method named render', function(){
+    expect(testList.render).to.exist;
+  });
+
+  it('should concatenate the results in the items arry, wrap data in ul tags , and return an html formatted string ', function(){
+
+    var item1 = new ShoppingListItem('eggs', 'protein');
+    var item2 = new ShoppingListItem('bacon', 'protein');
+    var item3 = 'mcflurry';
+
+    testList.addItem(item1);
+    testList.addItem(item2);
+    testList.removeItem(item1);
+    testList.removeItem(item2);
+
+    expect(testList.items.length).to.not.be.equal(2);
+    expect(testList.items).to.be.deep.equal([]);
+    (function(){testList.removeItem();}).should.not.throw(Error, 'CRAZY');
+    (function(){testList.removeItem(item3);}).should.throw(Error, 'CRAZY');
+
+  });
+
 });
